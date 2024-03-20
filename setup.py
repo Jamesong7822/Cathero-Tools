9e16e77f5634393a9f1941f5a32c09a7926d95b2
Wei Song <weisong_ong@mymail.sutd.edu.sg>
2024-03-20 23:12:04 +0800

    Need to downgrade tesserocr whl, update setup.py and task

--- setup.py
+++ setup.py
@@ -12,5 +12,4 @@ setup(
    author_email='weiong7822@gmail.com',
    packages=find_packages(),  #same as name
    install_requires=dependencies,
-   dependencies="".join(["file:\\", os.path.join(os.getcwd(), "tesserocr-2.6.2-cp312-cp312-win_amd64.whl")]),
 )
